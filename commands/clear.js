fed4f35050e49e4922c406bd1603fa647eac9d69
Andoch Bonin <andochbonin@gmail.com>
2021-03-07 19:28:40 +0000

    allowed everyone to use clear function

--- commands/clear.js
+++ commands/clear.js
@@ -3,11 +3,6 @@ module.exports = {
     description: "clears a specified number of messages",
     async execute(message, args) {
 
-        if (!message.member.hasPermission("MANAGE_MESSAGES")){
-            message.channel.send("You do not have the right geass for this.");
-            return;
-        }
-
         if (!args[0] || isNaN(args[0])) {
             return message.reply('Specify a number.');
         }
